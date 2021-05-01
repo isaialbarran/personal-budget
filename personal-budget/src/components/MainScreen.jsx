9e9e73b0ce35991d444812c2e7fcdc2f8bcddcbd
Isai Albarran <isaialbarran@gmail.com>
2021-05-01 12:50:15 +0200

    add link into mainscreen

--- personal-budget/src/components/MainScreen.jsx
+++ personal-budget/src/components/MainScreen.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
 export const MainScreen = () => {
+
   return (
     <div>
-      <h1>Main Screen</h1>
-      
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link" aria-current="page">Logo</a>
+              <a class="nav-link">Inicio</a>
+              <a class="nav-link">Movimientos</a>
+              <a class="nav-link">Análisis</a>
+              <a class="nav-link">Servicios</a>
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
