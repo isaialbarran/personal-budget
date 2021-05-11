6840ab8d6cfff0f34449e22698257d700fed19dd
Isai Albarran <isaialbarran@gmail.com>
2021-05-11 08:29:11 +0200

    update form values

--- personal-budget/src/components/main/movements/Movements.jsx
+++ personal-budget/src/components/main/movements/Movements.jsx
@@ -1,27 +1,40 @@
 import React, { useState, useEffect } from 'react'
-import './style.css'
-import mockTransactions from '../../../mocks/transactions'
 import { v4 as uuidv4 } from 'uuid';
+import { Spinner, Button, Container, Navbar, Modal, Form } from 'react-bootstrap';
+
+import mockTransactions from '../../../mocks/transactions'
+
+import { TableMovementComponent } from './TableComponent'
+
+import './style.css'
 
 const Movements = () => {
 
     const [transactions, setTransactions] = useState([])
+
     const [loading, setLoading] = useState(true)
 
-    const [form, setForm] = useState({})
-    const [errors, setErrors] = useState({})
+    const [show, setShow] = useState(false);
 
-    const setField = (field, value) => {
-        setForm({
-            ...form,
-            [field]: value
-        })
-        console.log("form", form);
-        if (!!errors[field]) setErrors({
-            ...errors,
-            [field]: null
-        })
-    }
+    const handleClose = () => {
+        setForm({})
+        setShow(false)
+    };
+    const handleShow = () => setShow(true);
+
+    const [form, setForm] = useState({
+        title: "",
+        date: "",
+        type: "",
+        amount: ""
+    })
+
+    const [formError, setFormError] = useState({
+        title: "",
+        date: "",
+        type: "",
+        amount: ""
+    })
 
     const fetchTransactions = async () => {
         setTimeout(() => {
@@ -32,68 +45,26 @@ const Movements = () => {
         console.log("transactions", transactions);
     };
 
-    const findFormErrors = () => {
-        const { date, title, description } = form
-        const newErrors = {}
-        // name errors
-        if (!title || title === '') newErrors.title = 'cannot be blank!'
-        else if (title.length > 30) newErrors.title = 'name is too long!'
-        // description errors
-        if (!description || description === '') newErrors.description = 'cannot be blank!'
-        else if (description.length > 100) newErrors.description = 'description is too long!'
-
-        console.log("errors", errors);
-
-        return newErrors
+    const checkFormError = () => {
+        console.log("hola :)");
+        if (form.title.length < 3) {
+            setFormError({ title: "Name to short" })
+        }
     }
 
-    const clearForm = () => {
-        document.getElementById("movementForm").reset();
-        const initialForm = {
-            id: "",
-            date: "",
-            title: "",
-            description: ""
-        }
-        setForm({ ...initialForm })
+    const handleChange = (e) => {
+        console.log("e", e.target.value);
+        console.log("e", e.target);
+        setForm({ ...form, [e.target.name]: e.target.value })
+        console.log("form", form);
     }
 
     const handleSubmit = e => {
         console.log("entro", e);
-        e.preventDefault()
-        // get our new errors
-        const newErrors = findFormErrors()
-        // Conditional logic:
-        if (Object.keys(newErrors).length > 0) {
-            // We got errors!
-            setErrors(newErrors)
-        } else {
-            form.id = uuidv4()
-            // No errors! Put any logic here for the form submission!
-            console.log("form before", form);
-
-            setTransactions([...transactions, form])
-            console.log("transactions", transactions);
-            console.log("form after", form);
-            clearForm()
-            alert('Thank you for your feedback!')
-        }
-    }
-
-    const deleteValue = (id) => {
-        const resp = transactions.filter(transaction => transaction.id !== id)
-        setTransactions(resp)
-    }
-
-    const editValue = (id) => {
-        const value = transactions.find(transaction => transaction.id === id)
-        console.log("value", value);
-        const objectToEdit = {
-            title: value.title,
-            description: value.description
-        }
-        setForm({ objectToEdit })
         console.log("form", form);
+        checkFormError()
+        e.preventDefault()
+        return;
     }
 
     useEffect(() => {
@@ -102,44 +73,88 @@ const Movements = () => {
 
     return (
         <div>
-            <div>
-                <h1 className="square">Movements</h1>
-            </div>
-            {loading ? <h1>Cargando</h1> : <div>
-                <ul>
-                    {transactions.map(transaction => (
-                        <li key={transaction.id}>
-                            <div>{transaction.date}{transaction.amount}</div>
-                            <div>{transaction.title}</div>
-                            <div>{transaction.description}</div>
-                            <button type="button" onClick={() => editValue(transaction.id)}>Edit</button>
-                            <button type="button" onClick={() => deleteValue(transaction.id)}>Delete</button><hr />
-                        </li>
-                    ))}
-                </ul>
-            </div>}
+            <Container>
+                <Navbar expand="lg" variant="light" bg="light">
+                    <Navbar.Brand href="#"><h1>Movements</h1></Navbar.Brand>
+                    {loading ? "" : <Button onClick={handleShow} variant="primary">Add movement</Button>}
+                </Navbar>
+                {loading ?
+                    <Container className="main h-500">
+                        <div className="spinner-layout">
+                            <Spinner animation="border" role="status"></Spinner>
+                        </div>
+                    </Container>
+                    : <div>
+                        <TableMovementComponent setTransactions={setTransactions} transactions={transactions}></TableMovementComponent>
+                    </div>
+                }
+                {show && <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Movement Form</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form>
+                            <Form.Group controlId="formBasicTitle">
+                                <Form.Label>Title</Form.Label>
+                                <Form.Control
+                                    name="title"
+                                    value={form.title}
+                                    onChange={handleChange}
+                                    type="text"
+                                    isInvalid={!!formError.title}
+                                    placeholder="Enter a title" />
+                                <Form.Control.Feedback type="invalid">
+                                    {formError.title}
+                                </Form.Control.Feedback>
+                                <Form.Text className="text-muted">
+                                    We'll never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
 
-            <form id="movementForm">
-                <div>
-                    <label>Title</label>
-                    <input
-                        type="text"
-                        placeholder="Title"
-                        onChange={e => setField('title', e.target.value)}
-                    />
-                    {errors.title ? <span>{errors.title}</span> : ""}
-                </div>
-                <div>
-                    <label>Description</label>
-                    <input
-                        type="text"
-                        placeholder="Description"
-                        onChange={e => setField('description', e.target.value)} />
-                    {errors.description ? <span>{errors.description}</span> : ""}
+                            <Form.Group controlId="formBasicDate">
+                                <Form.Label>Date</Form.Label>
+                                <Form.Control
+                                    name="date"
+                                    value={form.date}
+                                    onChange={handleChange}
+                                    type="date"
+                                    placeholder="Date" />
+                            </Form.Group>
 
-                </div>
-                <button variant="primary" type="submit" onClick={handleSubmit}>Submit</button>
-            </form>
+                            <Form.Group controlId="formBasicAmount">
+                                <Form.Label>Amount</Form.Label>
+                                <Form.Control
+                                    name="amount"
+                                    value={form.amount}
+                                    onChange={handleChange}
+                                    type="number"
+                                    placeholder="Enter the amount" />
+                                <Form.Text className="text-muted">
+                                    We'll never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicType">
+                                <Form.Label>Type</Form.Label>
+                                <Form.Control
+                                    name="type"
+                                    value={form.type}
+                                    onChange={handleChange}
+                                    as="select">
+                                    <option value="0">Choose...</option>
+                                    <option value="output">Output</option>
+                                    <option value="input">Income</option>
+                                </Form.Control>
+                            </Form.Group>
+
+                            <Button variant="primary" type="submit" onClick={handleSubmit}>
+                                Submit
+                            </Button>
+                        </Form>
+                    </Modal.Body>
+                </Modal>
+                }
+            </Container>
         </div>
     )
 }
